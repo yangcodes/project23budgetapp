c76fc4152cb3f592750eb496e3319b9d2e6ed70a
Jenny Lin <misslinyang91@gmail.com>
2021-08-26 14:35:39 +1000

    create hide-show dashboard

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const incomeTotalEl = document.querySelector(".income-total");
 const outcomeTotalEl = document.querySelector(".expense-total");
 const incomeEl = document.querySelector("#income-tracker");
 const expenseEl = document.querySelector("#expense-tracker");
-const allEl = document.querySelector("#all");
+constList = document.querySelector("#all");
 const incomeList = document.querySelector("#income-tracker .list");
 const expenseList = document.querySelector("#expense-tracker .list");
 const allList = document.querySelector("#all .list");
@@ -26,3 +26,51 @@ const incomeAmount = document.querySelector("#income-amount-input");
 let ENTRY_LIST = [];
 let [balance, income, outcome] = [0, 0, 0];
 let [deleteIcon, editIcon] = ["fas fa-trash", "far fa-edit"];
+
+//expenseBtn event listenrer
+expenseBtn.addEventListener("click", function () {
+  show(expenseEl);
+  hide([incomeEl, allList]);
+  active(expenseBtn);
+  inactive([incomeBtn, allBtn]);
+});
+
+//incomeBtn event listenrer
+incomeBtn.addEventListener("click", function () {
+  show(incomeEl);
+  hide([expenseEl, allList]);
+  active(incomeBtn);
+  inactive([expenseBtn, allBtn]);
+});
+
+//allBtn event listenrer
+allBtn.addEventListener("click", function () {
+  show(allList);
+  hide([incomeEl, expenseEl]);
+  active(allBtn);
+  inactive([incomeBtn, expenseBtn]);
+});
+
+//show function
+function show(element) {
+  element.classList.remove("hide");
+}
+
+//hide function
+function hide(elements) {
+  elements.forEach(function (element) {
+    element.classList.add("hide");
+  });
+}
+
+//active function
+function active(element) {
+  element.classList.add("active");
+}
+
+//inactive function
+function inactive(elements) {
+  elements.forEach(function (element) {
+    element.classList.remove("active");
+  });
+}
